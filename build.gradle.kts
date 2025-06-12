1040a22427666f459dafd11bf220126f9ef3cff9
vlad.lezhnin <vladislav.lezhnin@starhive.com>
2025-06-12 18:36:33 +0100

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+}
+
+group = "dev.codemorph"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
