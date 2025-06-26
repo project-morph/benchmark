b6b3f4980f25e63c3e561d3a985f914171e614ff
codewithbear <lezhnin.vlad@gmail.com>
2025-06-26 07:00:57 +0100

    enable test

--- src/test/kotlin/dev/codemorph/benchmark/TestYamlChangeValue.kt
+++ src/test/kotlin/dev/codemorph/benchmark/TestYamlChangeValue.kt
@@ -1,10 +1,8 @@
 package dev.codemorph.benchmark
 
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.TestFactory
 
 class TestYamlChangeValue {
     @TestFactory
-    @Disabled
     fun testYamlChangeValue() = TestRunner.runCase("yaml_change_value")
 }
