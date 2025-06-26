4ca96aaf3bc000365103ae27fec73bd127265621
codewithbear <lezhnin.vlad@gmail.com>
2025-06-26 16:11:28 +0100

    Morph: switch to Unleash with Sonnet 4

--- src/test/java/dev/codemorph/benchmark/unleash/CostCenterServiceTest.java
+++ src/test/java/dev/codemorph/benchmark/unleash/CostCenterServiceTest.java
@@ -1,13 +1,19 @@
 package dev.codemorph.benchmark.unleash;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import io.getunleash.Unleash;
 import org.junit.jupiter.api.Test;
 
 class CostCenterServiceTest {
   @Test
   void addCostCenter() {
-    var instance = new CostCenterService();
+    Unleash mockUnleash = mock(Unleash.class);
+    when(mockUnleash.isEnabled(anyString())).thenReturn(true);
+    var instance = new CostCenterService(mockUnleash);
     assertEquals("Cost center added: IT", instance.addCostCenter("IT", 1000.0));
   }
 }
