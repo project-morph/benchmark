4efd1a566fca00f0012e8517a27a33f0cd2a8c7f
codewithbear <lezhnin.vlad@gmail.com>
2025-06-26 10:35:07 +0100

    Morph: switch to unleash for feature flags

--- src/test/java/dev/codemorph/benchmark/unleash/TaskServiceActualTest.java
+++ src/test/java/dev/codemorph/benchmark/unleash/TaskServiceActualTest.java
@@ -1,13 +1,19 @@
 package dev.codemorph.benchmark.unleash;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+import io.getunleash.Unleash;
 import org.junit.jupiter.api.Test;
 
 class TaskServiceActualTest {
 
   @Test
   void getRelevantTaskIds() {
-    var instance = new TaskServiceActual();
+    Unleash mockUnleash = mock(Unleash.class);
+    when(mockUnleash.isEnabled("relevant-tasks")).thenReturn(true);
+
+    var instance = new TaskServiceActual(mockUnleash);
 
     assertEquals(3, instance.getRelevantTaskIds().size());
   }
