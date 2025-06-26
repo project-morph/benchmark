c131f066454e79f33b61538e5a5ba09132193adb
codewithbear <lezhnin.vlad@gmail.com>
2025-06-26 13:01:52 +0100

    enable unleash test

--- src/test/kotlin/dev/codemorph/benchmark/TestJavaUnleashFeatureFlags.kt
+++ src/test/kotlin/dev/codemorph/benchmark/TestJavaUnleashFeatureFlags.kt
@@ -1,11 +1,9 @@
 package dev.codemorph.benchmark
 
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.TestFactory
 
 class TestJavaUnleashFeatureFlags {
     @TestFactory
-    @Disabled
     fun testJavaUnleashFeatureFlags() = TestRunner.runCaseJava(
         "src/main/java/dev/codemorph/benchmark/unleash/expected",
         "src/main/java/dev/codemorph/benchmark/unleash"
